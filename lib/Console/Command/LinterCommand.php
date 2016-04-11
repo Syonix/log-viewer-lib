bbb447ce466add035a0abc701308d2c29d28c2eb
Michael Burri <login@michaelburri.ch>
2016-04-11 11:59:49 +0200

    Update LinterCommand.php

--- lib/Console/Command/LinterCommand.php
+++ lib/Console/Command/LinterCommand.php
@@ -3,14 +3,18 @@ namespace Syonix\LogViewer\Console\Command;
 
 class LintCommand extends Command
 {
-    protected function configure()
+    private $configDefaultPath;
+    
+    protected function configure($configDefaultPath = null)
     {
+        $this->configDefaultPath = $configDefaultPath;
+        
         $this
             ->setName('config:lint')
             ->setDescription('Lint your config file to detect potential problems')
             ->addArgument(
                 'config_file',
-                InputArgument::REQUIRED,
+                InputArgument::OPTIONAL,
                 'The path to your config file'
             )
         ;
@@ -19,5 +23,6 @@ class LintCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // TODO: Implement
+        // TODO: If config file not given, search in $this->configDefaultPath
     }
 }
